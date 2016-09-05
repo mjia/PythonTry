8db3d5fc4d53a1d7e891ca51e874db356a20cd11
Michael Jia <jia.mike@gmail.com>
2016-09-05 17:11:23 +0800

    tryeed http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/001386818214042e9c46552422e42d8a00f019e088506ce000

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,7 @@
-#! /usr/bin/python 
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-print "hello, 你好"  
+print 'The quick brown fox','jumps over','the lazy dog'
+print 100+200
+name = raw_input("Please enter your 名字:")
+print name
